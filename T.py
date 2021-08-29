7f4e215ba6b3c84efede323aad6c64865a9a3e2b
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-29 19:46:43 +0530

    Update T.py

--- T.py
+++ T.py
@@ -16,14 +16,7 @@ tim = Turtle()
 
 
 
-'''Draw a dashed line'''
 
-# for i in range(15):
-
-#     tim.forward(10)
-#     tim.penup()
-#     tim.forward(10)
-#     tim.pendown()
 
 '''Draw different shapes'''
 
